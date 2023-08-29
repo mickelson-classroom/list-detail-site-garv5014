1f5164a94b4a582884120462e0d622d7c3601c43
garv5014 <garv5014@gmail.com>
2023-08-29 15:57:32 -0600

    added the delete Item functionality

--- list-details/src/components/MasterList.tsx
+++ list-details/src/components/MasterList.tsx
@@ -5,15 +5,23 @@ import { ItemsList } from "./ItemsList";
 import { AddItem } from "./AddItem";
 
 export const MasterList = () => {
-  let items = [ { id: 1, name: "Item 1" , details: "an example Item"}, { id: 2, name: "Item 2", details: "This is an example Item" } ];
+  let i = [ { id: 1, name: "Item 1" , details: "an example Item"}, { id: 2, name: "Item 2", details: "This is an example Item" } ];
 
+
+  const [items, setItems] = React.useState(i);
   const [listIndex, setListIndex] = React.useState(0);
 
   const handleListClick = (item:number) => {
     setListIndex(item);
   }
 
-  const handleDelete = () => {}
+  const handleDelete = () => {
+    let newList = items.filter((_, index) => {
+      return index !== listIndex;
+    })
+    setItems(newList);
+    setListIndex(0);
+  }
   // the onclick event from the list item sends back target index for the details
   // the onclick/delete event from the details the item at the index. 
   return (
