5829903641d6d2b114696c288fdd7c422c3ea64b
garv5014 <garv5014@gmail.com>
2023-09-06 14:13:51 -0600

    changed the primary color

--- list-details/src/index.tsx
+++ list-details/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/custom.scss";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
